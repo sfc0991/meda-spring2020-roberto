02a7f070d7e736423cbf985a5427fca79316fbfc
Roberto Escobar <robertoescobar@Robertos-MacBook-Pro.local>
2020-02-25 12:41:32 -0800

    Worked on Javascript files and added some new folders and files

---
+++ practice/javascript/week_6/third.js
@@ -0,0 +1,61 @@
+var myFirstVar = 1000;
+
+var yearsLeft = 1000;
+
+// Units in meters.
+var buildingHeightMeters = 100;
+
+var humanAge = 16;
+
+var humanAgeDays = 5840;
+
+// Bad name.
+var x = "Super Important";
+
+// Equal sign, everything on the right always happens first.
+var sum = 1000 + 50;
+
+var product = 50 * 50;
+
+var dividend = 20 / 10;
+
+var difference = 100 - 10;
+
+// When using variable names on the right side of the equal sign, you are calling for value and then you are runnin the opperators, and finally you are storing the final value into the varuable total.
+var total = sum + product;
+
+console.log (total);
+
+
+// Looked at the built-in Math Library of Javascript.
+var radius =40;
+
+var diameterOfCircle = 2 * radius;
+var perimeterOfCircle = Math.PI * diameterOfCircle;
+
+
+console.log("The diameter of the circle with a radius of " + radius + " is " + diameterOfCircle + ". The perimeter of the circle is " + perimeterOfCircle);
+
+
+
+// Rounds to the nearest whole number.
+var rounded = Math.round(3.14);
+console.log(rounded);
+
+// Round up.
+var roundedUp = Math.ceil(45.1);
+
+// Round down.
+var roundedDown = Math.floor(45.9); 
+
+// Math.random() returns a value between 0 and .9infinity
+// We add a 1 so minimum is now 1, and we miltiply 1000 so maximum is 1000.9.
+var someNumber = ( Math.random() * 1000 ) + 1;
+
+// We floor the random nu,ber so decimals are removed.
+var roundedRandomNumber = Math.floor(someNumber);
+// or use the existing variable:
+// someNumber = Math.floor(someNumber);
+
+// Print random rumber.
+console.log(roundedRandomNumber);
